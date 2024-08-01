bc367d11c06bc6105f552ee8399cf16b8f43cfb9
Taha Adeel Mohammed <taha.m@media.net>
2024-08-01 20:34:49 +0530

    Hint end-point done

--- Code/Backend/controller.js
+++ Code/Backend/controller.js
@@ -1,7 +1,11 @@
 const hintsService = require('./chat-bot/hints');
 
-exports.getHint = (req, res) => {
-  const inputData = req.body;
-  const result = hintsService.execute(inputData);
-  res.json(result);
+exports.getHint = async (req, res) => {
+  const { problem_statement, editorial } = req.body;
+  console.log('Received Hint Request');
+  console.log('Problem Statement:\n', problem_statement);
+  console.log('Editorial:\n', editorial);
+
+  const result = await hintsService.execute(problem_statement, editorial);
+  res.status(201).json(result);
 };
